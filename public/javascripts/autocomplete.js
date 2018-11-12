8cbf99f78c67dac61bec8a0671f40a0638a8b34d
최아연 <ayon.c@ridi.com>
2018-11-13 04:08:52 +0900

    initial commit

---
+++ public/javascripts/autocomplete.js
@@ -0,0 +1,28 @@
+const { fromEvent } = rxjs;
+const { map, mergeMap, debounceTime, filter, distinctUntilChanged } = rxjs.operators;
+const { ajax } = rxjs.ajax;
+
+const $layer = document.getElementById("suggestLayer");
+
+function drawLayer(items) {
+  $layer.innerHTML = items.map(user => {
+    return `<li class="user">
+    <img src="${user.avatar_url}" width="50px" height="50px"/>
+    <p><a href="${user.html_url}" target="_blank">${user.login}</a></p>
+    </li>`
+  }).join("");
+}
+
+
+const user$ = fromEvent(document.getElementById("search"), "keyup")
+  .pipe(
+    debounceTime(300), // 300ms 뒤에 데이터를 전달한다.
+    map(event => event.target.value),
+    distinctUntilChanged(),  // 특수키가 입력된 경우에는 나오지 않기 위해 중복 데이터 처리
+    filter(query => query.trim().length > 0),
+    mergeMap(query => ajax.getJSON(`https://api.github.com/search/users?q=${query}`)),
+  );
+
+user$.subscribe(v => {
+  drawLayer(v.items);
+});
